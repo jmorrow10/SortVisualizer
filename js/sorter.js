231cb8a17721f3b6f28ff1e559dcd0a5f1649e2a
VALENTINE <valentine@unl.edu>
2018-10-01 18:00:12 -0500

    minor adjustment to comments in mergeSort function

--- js/sorter.js
+++ js/sorter.js
@@ -22,15 +22,6 @@ function generateRandomArray(n){
  * array.
  */
 function mergeSort(arr){	
-	// test to see if the subarray under consideration is of length one
-	// if so, return the subarray
-	
-	// find pivot point (arr.length/2)
-
-	// recurse on left half of the subarry and store result (use JS slice function)
-	// recurse on right half of the subarray and store result (use JS slice function)
-	// merge the stored, sorted left and right halves together.
-	// return the merged array
 	return [];
 }
 
